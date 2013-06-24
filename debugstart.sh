9a726fd56812dc9fe17a514663a72a96a3fa52f3
Paul Thornton <Ariochofmelnibone@gmail.com>
2013-06-24 21:40:35 +0100

    added compile step

--- debugstart.sh
+++ debugstart.sh
@@ -1,4 +1,6 @@
 dir;
 dir ./web/server/client/;
+./dart-sdk/bin/dart2js --out web/server/client/bundle.js web/server/client/networkedsolarclient.dart
+echo "compiled client to js";
 
 ./dart-sdk/bin/dart web/server/solarserver.dart
