620399ce97191121c6b2db70464d0a0fcbb5cef2
Paul Thornton <Ariochofmelnibone@gmail.com>
2013-06-24 21:53:30 +0100

    Fixed BuildScript
    Rawr!!

--- debugstart.sh
+++ debugstart.sh
@@ -1,6 +1,6 @@
 dir;
 dir ./web/server/client/;
-./dart-sdk/bin/dart2js -o web/server/client/bundle.js web/server/client/networkedsolarclient.dart
+./dart-sdk/bin/dart2js -o web/server/client/networkedsolarclient.dart.js web/server/client/networkedsolarclient.dart
 echo "compiled client to js (Attempt 2)";
 
 ./dart-sdk/bin/dart web/server/solarserver.dart
