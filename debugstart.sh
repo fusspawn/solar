ca58c941a21cc16e1b26de5c3ddff2c8d208f1e3
Paul Thornton <Ariochofmelnibone@gmail.com>
2013-06-24 21:48:29 +0100

    asd
    asd

--- debugstart.sh
+++ debugstart.sh
@@ -1,6 +1,6 @@
 dir;
 dir ./web/server/client/;
 ./dart-sdk/bin/dart2js -o web/server/client/bundle.js web/server/client/networkedsolarclient.dart
-echo "compiled client to js";
+echo "compiled client to js (Attempt 2)";
 
 ./dart-sdk/bin/dart web/server/solarserver.dart
